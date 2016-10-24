9a5fa6887e050dda7ae6d36f3bb6af3a8b866047
Paul <pfanone@gmail.com>
2016-10-24 17:01:42 -0400

    updating structure

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layout/main')
+@section('title', 'Paul Fanone')
+
+@section('content')
+<div class="container">
+	<div class="content">
+		<div class="title">Paul Fanone</div>
+		<div class="sub-title">Web Developer</div>
+	</div>
+</div>
+@endsection
---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta name="keywords" content="web developer">
+	<meta name="description" content="Paul Fanone's Personal Website">
+	<title>@yield('title')</title>
+	<link rel="shortcut icon" href="{{\Config::get('branding.icon')}}" />
+	<link rel="apple-touch-icon" href="{{\Config::get('branding.touch_icon')}}" />
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+	{{ HTML::style('css/main.css') }}
+</head>
+
+<body>
+	<div class="container">
+		@yield('content')
+	</div>
+</body>
+</html>
