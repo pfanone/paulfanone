105516172d47bdbe1a40727160bde0ab4776b098
Paul <pfanone@gmail.com>
2016-10-24 17:03:20 -0400

    fixing include

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -9,8 +9,8 @@
 	<title>@yield('title')</title>
 
 	<link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-	
-	{{ HTML::style('css/main.css') }}
+
+	{!! HTML::style('/css/main.css') !!}
 </head>
 
 <body>
