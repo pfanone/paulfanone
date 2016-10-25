f6fde955730b1f72a836079fff99a56d2cbd3012
Paul <pfanone@gmail.com>
2016-10-25 11:42:30 -0400

    styling

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -14,9 +14,9 @@
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-xs-12 col-sm-3 text-right">
-			<p>About Paul</p>
+			<h3>About Paul</h3>
 		</div>
-		<div class="col-xs-12 col-sm-9">
+		<div class="col-xs-12 col-sm-9 pad30">
 			<ul>
 				<li>Ryerson University Computer Science Graduate.</li>
 				<li>Web Developer at WhoPlusYou.com</li>
