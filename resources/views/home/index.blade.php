db9f4e6aba0977ef95ea9305e473d4dafb50742f
Paul <pfanone@gmail.com>
2016-10-24 23:43:59 -0400

    updating structure

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,14 +4,16 @@
 @section('content')
 
 
-<div class="parallax-container" data-parallax="scroll" data-position="top" data-bleed="10" data-image-src="./img/stellar-spire-eagle-nebula-1400x900.jpg" data-natural-width="1400" data-natural-height="900" style="height: 280px;"></div>
+<div class="parallax-container" data-parallax="scroll" data-position="top" data-bleed="10" data-image-src="/img/stellar-spire-eagle-nebula-1400x900.jpg" data-natural-width="1400" data-natural-height="900" style="height: 280px;"></div>
+
+<section>
 	<div class="row">
-		<div class="col-xs-12">
+		<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-2 col-md-offset-5">
 			<div class="title">Paul Fanone</div>
 			<div class="sub-title">Web Developer</div>
 		</div>
 	</div>
-</div>
+</section>
 
 
 @endsection
