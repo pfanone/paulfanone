48d42b21457afb57320a4af22251de811c49a2a4
Paul <pfanone@gmail.com>
2016-10-25 10:50:14 -0400

    adding helper classes

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,7 +5,7 @@
 
 <div class="container-fluid title-div">
 	<div class="row">
-		<div class="col-xs-12 text-center">
+		<div class="col-xs-12 text-center padT80">
 			<div class="title">Paul Fanone</div>
 		</div>
 	</div>
