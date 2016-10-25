4e91dfa45ef3ba0ee7c48ae09951c430ac656476
Paul <pfanone@gmail.com>
2016-10-25 00:36:19 -0400

    switching div type

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -9,8 +9,7 @@
 			<div class="hsContent" data-center="opacity: 1" data-106-top="opacity: 0" data-anchor-target="#slide-1 h2">
 				<div class="row">
 					<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
-						<div class="title">Paul Fanone</div>
-						<div class="sub-title">Web Developer</div>
+						<hr>Paul Fanone</div>
 					</div>
 				</div>
 			</div>
