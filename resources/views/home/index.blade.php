f5c997f46348ab24ec9721fe25ba3d120928345a
Paul <pfanone@gmail.com>
2016-10-25 00:38:23 -0400

    testing

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,11 +7,7 @@
 	<div class="bcg" data-center="background-position: 50% 0px;" data-top-bottom="background-position: 50% -100px;" data-anchor-target="#slide-1">
 		<div class="hsContainer">
 			<div class="hsContent" data-center="opacity: 1" data-106-top="opacity: 0" data-anchor-target="#slide-1 h2">
-				<div class="row">
-					<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
-						<h2>Paul Fanone</h2>
-					</div>
-				</div>
+				<h2>Paul Fanone</h2>
 			</div>
 		</div>
 	</div>
