d1e447cccc75e9de2733af953603b34d1858ee3d
Paul <pfanone@gmail.com>
2016-10-25 10:14:30 -0400

    updates

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,7 +5,7 @@
 
 <div class="container-fluid title-div">
 	<div class="row">
-		<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4 text-center">
+		<div class="col-xs-12 text-center">
 			<div class="title">Paul Fanone</div>
 		</div>
 	</div>
