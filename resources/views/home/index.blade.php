471c4ee61a989529f9fbc784bfd46afae600d6c8
Paul <pfanone@gmail.com>
2016-10-25 00:07:14 -0400

    testing

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -14,4 +14,35 @@
 
 <div class="parallax-window" data-parallax="scroll" data-image-src="/img/stellar-spire-eagle-nebula-1400x900.jpg"></div>
 
+<section>
+	<div class="container">
+		<div class="text-center">
+			<div class="title">Paul Fanone</div>
+			<div class="sub-title">Web Developer</div>
+		</div>
+	</div>
+</section>
+
+<div class="parallax-window" data-parallax="scroll" data-image-src="/img/stellar-spire-eagle-nebula-1400x900.jpg"></div>
+
+<section>
+	<div class="container">
+		<div class="text-center">
+			<div class="title">Paul Fanone</div>
+			<div class="sub-title">Web Developer</div>
+		</div>
+	</div>
+</section>
+
+<div class="parallax-window" data-parallax="scroll" data-image-src="/img/stellar-spire-eagle-nebula-1400x900.jpg"></div>
+
+<section>
+	<div class="container">
+		<div class="text-center">
+			<div class="title">Paul Fanone</div>
+			<div class="sub-title">Web Developer</div>
+		</div>
+	</div>
+</section>
+
 @endsection
