47e206115e515c47703d4ee292ab571bd0314040
Paul <pfanone@gmail.com>
2016-10-24 23:38:13 -0400

    testing parallax

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,10 +2,16 @@
 @section('title', 'Paul Fanone')
 
 @section('content')
-<div class="main-container">
-	<div class="content">
-		<div class="title">Paul Fanone</div>
-		<div class="sub-title">Web Developer</div>
+
+
+<div class="parallax-container" data-parallax="scroll" data-position="top" data-bleed="10" data-image-src="./img/stellar-spire-eagle-nebula-1400x900.jpg" data-natural-width="1400" data-natural-height="900" style="height: 280px;"></div>
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="title">Paul Fanone</div>
+			<div class="sub-title">Web Developer</div>
+		</div>
 	</div>
 </div>
+
+
 @endsection
