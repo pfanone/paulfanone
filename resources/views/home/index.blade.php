0fb6b5d8886e35f866cc69426ba44cdf3d4db557
Paul <pfanone@gmail.com>
2016-10-24 23:53:02 -0400

    reverting to parallax

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,14 +3,16 @@
 
 @section('content')
 
-<div data-0="background-color:rgb(0,0,255);" data-500="background-color:rgb(255,0,0);">
+<section>
 	<div class="row">
-		<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-2 col-md-offset-5">
+		<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4 text-center">
 			<div class="title">Paul Fanone</div>
 			<div class="sub-title">Web Developer</div>
 		</div>
 	</div>
-</div>
+</section>
+
+<div class="parallax-container" data-parallax="scroll" data-position="top" data-bleed="10" data-image-src="./img/stellar-spire-eagle-nebula-1400x900.jpg" data-natural-width="1400" data-natural-height="900" style="height: 280px;"></div>
 
 
 @endsection
