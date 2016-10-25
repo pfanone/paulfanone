c2c40278f7ae4f0915164b0f29b676809da3311c
Paul <pfanone@gmail.com>
2016-10-25 00:51:09 -0400

    fixing

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
 
-<div id="title-div" class="container">
+<div class="container title-div">
 	<div class="row">
 		<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4 text-center">
 			<div class="title">Paul Fanone</div>
