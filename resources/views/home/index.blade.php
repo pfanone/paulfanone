d011da621aec988437caf24e1036f32309e2547b
Paul <pfanone@gmail.com>
2016-10-25 11:40:26 -0400

    adding basic resume elements

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -11,4 +11,18 @@
 	</div>
 </div>
 
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-xs-12 col-sm-3 text-right">
+			<p>About Paul</p>
+		</div>
+		<div class="col-xs-12 col-sm-9">
+			<ul>
+				<li>Ryerson University Computer Science Graduate.</li>
+				<li>Web Developer at WhoPlusYou.com</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
 @endsection
