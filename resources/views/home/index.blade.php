9ede85ca99158281435a8797da384dbe2baac3e1
Paul <pfanone@gmail.com>
2016-10-25 00:34:57 -0400

    uploading images

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -18,7 +18,7 @@
 	</div>
 </section>
 
-<section id="slide-1" class="homeSlide">
+<section id="slide-2" class="homeSlide">
 	<div class="bcg" data-center="background-position: 50% 0px;" data-top-bottom="background-position: 50% -100px;" data-anchor-target="#slide-1">
 		<div class="hsContainer">
 			<div class="hsContent" data-center="opacity: 1" data-106-top="opacity: 0" data-anchor-target="#slide-1 h2">
@@ -33,7 +33,7 @@
 	</div>
 </section>
 
-<section id="slide-1" class="homeSlide">
+<section id="slide-3" class="homeSlide">
 	<div class="bcg" data-center="background-position: 50% 0px;" data-top-bottom="background-position: 50% -100px;" data-anchor-target="#slide-1">
 		<div class="hsContainer">
 			<div class="hsContent" data-center="opacity: 1" data-106-top="opacity: 0" data-anchor-target="#slide-1 h2">
