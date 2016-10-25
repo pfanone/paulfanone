249f1066d3995235cf7e12a0b590318b3c215f76
Paul <pfanone@gmail.com>
2016-10-25 00:37:24 -0400

    dumb

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -9,7 +9,7 @@
 			<div class="hsContent" data-center="opacity: 1" data-106-top="opacity: 0" data-anchor-target="#slide-1 h2">
 				<div class="row">
 					<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
-						<hr>Paul Fanone</div>
+						<h2>Paul Fanone</h2>
 					</div>
 				</div>
 			</div>
