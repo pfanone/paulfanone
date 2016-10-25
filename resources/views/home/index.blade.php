e4fd586c7e2cc571f7e94cc60825d572583cbc2a
Paul <pfanone@gmail.com>
2016-10-25 15:49:33 -0400

    updating formatting

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -11,24 +11,24 @@
 	</div>
 </div>
 
-<div class="container-fluid marginT20">
+<div class="container-fluid padT50 padB50">
 	<div class="row marginT20">
 		<div class="col-xs-12 col-sm-3 col-sm-offset-1 col-md-2 col-md-offset-2 text-center">
 			<h2 class="about">About Paul</h2>
 		</div>
-		<div class="col-xs-12 col-sm-6">
-			<p class="about">Offering creative design solutions to solve business problems is what I do best.</p>
-			<p class="about">I'm an experienced programmer many languaged including PHP, Javascript, MySQL, HTML, and CSS. I'm also open to learning new languages if need be.</p>
+		<div class="col-xs-12 col-sm-7 col-md-6">
+			<p class="about">I'm an adaptable programmer that can create specialized to meet any requirement.</p>
+			<p class="about">Experience in many languaged including PHP, Javascript, MySQL, HTML, and CSS. I'm also open to learning new languages as needed.</p>
 		</div>
 	</div>
 </div>
 
-<div class="container-fluid blue-bg marginT20 padT30 padB30">
+<div class="container-fluid blue-bg padT50 padB50">
 	<div class="row marginT20">
 		<div class="col-xs-12 col-sm-3 col-sm-offset-1 col-md-2 col-md-offset-2 text-center">
 			<h2 class="about">Experience</h2>
 		</div>
-		<div class="col-xs-12 col-sm-6">
+		<div class="col-xs-12 col-sm-7 col-md-6">
 			<p class="about">WhoPlusYou.com</p>
 			<p class="about">Ryerson University</p>
 		</div>
