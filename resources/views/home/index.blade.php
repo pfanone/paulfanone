e067ab45c0480bfdf0fd5c23216ef00805588ca0
Paul <pfanone@gmail.com>
2016-10-25 15:38:18 -0400

    updates

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -17,10 +17,20 @@
 			<h2 class="about">About Paul</h2>
 		</div>
 		<div class="col-xs-12 col-sm-6">
-			<ul class="cred-list marginT15">
-				<li>Ryerson University Computer Science Graduate</li>
-				<li>Web Developer at WhoPlusYou.com</li>
-			</ul>
+			<p class="about">Offering creative design solutions to solve business problems is what I do best.</p>
+			<p class="about">I'm an experienced programmer many languaged including PHP, Javascript, MySQL, HTML, and CSS. I'm also open to learning new languages if need be.</p>
+		</div>
+	</div>
+</div>
+
+<div class="container-fluid blue-bg marginT20 padT30 padB30">
+	<div class="row marginT20">
+		<div class="col-xs-12 col-sm-3 col-sm-offset-1 col-md-2 col-md-offset-2 text-center">
+			<h2 class="about">Experience</h2>
+		</div>
+		<div class="col-xs-12 col-sm-6">
+			<p class="about">WhoPlusYou.com</p>
+			<p class="about">Ryerson University</p>
 		</div>
 	</div>
 </div>
