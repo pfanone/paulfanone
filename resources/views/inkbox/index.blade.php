5b3bbb933dfdd824c598c8ebb7a9d0f9bd712920
Paul <pfanone@gmail.com>
2016-11-16 00:42:58 -0500

    updating colours

--- resources/views/inkbox/index.blade.php
+++ resources/views/inkbox/index.blade.php
@@ -8,7 +8,7 @@
 <div class="container-fluid">
 	<div class="row main-bg">
 		<div class="col-xs-12 main-header">
-			<h1 class="padL15">test</h1>
+			<h1 class="padL15">Inkbox Analytics</h1>
 		</div>
 	</div>
 
@@ -26,26 +26,17 @@
 		$("#user_graph").html('<canvas id="user_data_chart" width="400" height="400"></canvas>');
 		var ctx = document.getElementById("user_data_chart");
 		var myChart = new Chart(ctx, {
+			label: ""
 			type: 'bar',
 			data: {
 				labels: data['interval_array'],
 				datasets: [{
 					data: data['count_array'],
 					backgroundColor: [
-						'rgba(255, 99, 132, 0.2)',
-						'rgba(54, 162, 235, 0.2)',
-						'rgba(255, 206, 86, 0.2)',
-						'rgba(75, 192, 192, 0.2)',
-						'rgba(153, 102, 255, 0.2)',
-						'rgba(255, 159, 64, 0.2)'
+						'rgba(54, 162, 235, 0.2)'
 					],
 					borderColor: [
-						'rgba(255, 99, 132, 1)',
-						'rgba(54, 162, 235, 1)',
-						'rgba(255, 206, 86, 1)',
-						'rgba(75, 192, 192, 1)',
-						'rgba(153, 102, 255, 1)',
-						'rgba(255, 159, 64, 1)'
+						'rgba(54, 162, 235, 1)'
 					],
 					borderWidth: 1
 				}]
