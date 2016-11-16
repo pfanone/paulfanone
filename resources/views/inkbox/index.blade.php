5c644e01913c1f2ef9261ea446aeb98e39145b67
Paul <pfanone@gmail.com>
2016-11-15 22:46:22 -0500

    adding layout

--- resources/views/inkbox/index.blade.php
+++ resources/views/inkbox/index.blade.php
@@ -1,5 +1,13 @@
+@extends('layout/main')
+@section('title', 'Inkbox Analytics')
+
 @section("content")
 
-{{ json_encode($designs) }}
+<div class="row">
+	<div class="col-xs-12"><h1>test</h1></div>
+	<div class="col-xs-12">
+		{{ json_encode($designs) }}
+	</div>
+</div>
 
 @endsection
