23a6d5293fd2ae07d50e1c3a8a513764f1689221
Paul <pfanone@gmail.com>
2016-11-16 00:13:16 -0500

    testing background image

--- resources/views/inkbox/index.blade.php
+++ resources/views/inkbox/index.blade.php
@@ -3,10 +3,21 @@
 
 @section("content")
 
+<link rel="stylesheet" href="{{ URL::asset('css/inkbox.css') }}" />
+
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-xs-12"><h1>test</h1></div>
-		<div class="col-xs-12">
+		<div class="col-xs-12 col-sm-6 col-md-3">
+			<div class="well" id="user_graph"></div>
+		</div>
+		<div class="col-xs-12 col-sm-6 col-md-3">
+			<div class="well" id="tattoo_graph"></div>
+		</div>
+		<div class="col-xs-12 col-sm-6 col-md-3">
+			<div class="well" id="tattoo_user_graph"></div>
+		</div>
+		<div class="col-xs-12 col-sm-6 col-md-3">
 			<div class="well" id="user_graph"></div>
 		</div>
 	</div>
@@ -19,40 +30,39 @@
 		$("#user_graph").html('<canvas id="user_data_chart" width="400" height="400"></canvas>');
 		var ctx = document.getElementById("user_data_chart");
 		var myChart = new Chart(ctx, {
-		    type: 'bar',
-		    data: {
-		        labels: data['interval_array'],
-		        datasets: [{
-		            label: '# of Votes',
-		            data: data['count_array'],
-		            backgroundColor: [
-		                'rgba(255, 99, 132, 0.2)',
-		                'rgba(54, 162, 235, 0.2)',
-		                'rgba(255, 206, 86, 0.2)',
-		                'rgba(75, 192, 192, 0.2)',
-		                'rgba(153, 102, 255, 0.2)',
-		                'rgba(255, 159, 64, 0.2)'
-		            ],
-		            borderColor: [
-		                'rgba(255,99,132,1)',
-		                'rgba(54, 162, 235, 1)',
-		                'rgba(255, 206, 86, 1)',
-		                'rgba(75, 192, 192, 1)',
-		                'rgba(153, 102, 255, 1)',
-		                'rgba(255, 159, 64, 1)'
-		            ],
-		            borderWidth: 1
-		        }]
-		    },
-		    options: {
-		        scales: {
-		            yAxes: [{
-		                ticks: {
-		                    beginAtZero:true
-		                }
-		            }]
-		        }
-		    }
+			type: 'bar',
+			data: {
+				labels: data['interval_array'],
+				datasets: [{
+					data: data['count_array'],
+					backgroundColor: [
+						'rgba(255, 99, 132, 0.2)',
+						'rgba(54, 162, 235, 0.2)',
+						'rgba(255, 206, 86, 0.2)',
+						'rgba(75, 192, 192, 0.2)',
+						'rgba(153, 102, 255, 0.2)',
+						'rgba(255, 159, 64, 0.2)'
+					],
+					borderColor: [
+						'rgba(255, 99, 132, 1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)',
+						'rgba(75, 192, 192, 1)',
+						'rgba(153, 102, 255, 1)',
+						'rgba(255, 159, 64, 1)'
+					],
+					borderWidth: 1
+				}]
+			},
+			options: {
+				scales: {
+					yAxes: [{
+						ticks: {
+							beginAtZero:true
+						}
+					}]
+				}
+			}
 		});
 		console.log("user_data", data);
 	});
