cba91eeea76414f95e16efe7d2036562babb9358
Paul <pfanone@gmail.com>
2016-11-15 21:12:31 -0500

    testing db connection

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,11 +9,14 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesResources;
 use Illuminate\Http\Request;
 
+use DB;
 use View;
 
 class HomeController extends BaseController
 {
 	public function anyIndex() {
+		$select = DB::select("SELECT 1;", array());
+
 		return View::make('home.index');
 	}
 }
