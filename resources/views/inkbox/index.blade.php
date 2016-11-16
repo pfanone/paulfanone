79808570c1f7f8e370ace1a2b19d02967e61643d
Paul <pfanone@gmail.com>
2016-11-16 00:56:20 -0500

    graph change

--- resources/views/inkbox/index.blade.php
+++ resources/views/inkbox/index.blade.php
@@ -26,7 +26,7 @@
 		$("#user_graph").html('<canvas id="user_data_chart" width="400" height="400"></canvas>');
 		var ctx = document.getElementById("user_data_chart");
 		var myChart = new Chart(ctx, {
-			type: 'bar',
+			type: 'line',
 			data: {
 				labels: data['interval_array'],
 				datasets: [{
