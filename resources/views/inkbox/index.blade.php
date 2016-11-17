e9829777370515fc840d951403835f0b8e892e85
Paul <pfanone@gmail.com>
2016-11-17 10:53:12 -0500

    adding color and adjusting spacing

--- resources/views/inkbox/index.blade.php
+++ resources/views/inkbox/index.blade.php
@@ -14,10 +14,10 @@
 				</div>
 				<div class="col-xs-12 col-sm-6">
 					<div class="row">
-						<div class="col-xs-12 col-sm-3">
+						<div class="col-xs-12 col-sm-6">
 							<button id="graph_btn" class="btn-block graph-btn">Show Graphs</button>
 						</div>
-						<div class="col-xs-12 col-sm-3">
+						<div class="col-xs-12 col-sm-6">
 							<button id="tattoo_btn" class="btn-block graph-btn">Show Tattoos</button>
 						</div>
 					</div>
