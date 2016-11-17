70e2d7cf888122e8f8b9c63034ebb13080e35c50
Paul <pfanone@gmail.com>
2016-11-17 15:11:43 -0500

    update

--- resources/views/inkbox/partials/tattoo.blade.php
+++ resources/views/inkbox/partials/tattoo.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
 @foreach($tattoos as $key => $tattoo)
 	<div class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
-		<div class="well">
+		<div class="graph-div">
 			{!! json_encode($tattoo) !!}
 		</div>
 	</div>
