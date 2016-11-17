175791d34b267dca943fca30475518d02897be93
Paul <pfanone@gmail.com>
2016-11-17 16:55:48 -0500

    updating for activeonly

--- app/Http/Controllers/InkboxController.php
+++ app/Http/Controllers/InkboxController.php
@@ -205,7 +205,7 @@ class InkboxController extends BaseController
 		$return_array = array();
 		$tattoo_array = array();
 
-		$select = DB::select('SELECT `id`, `user_id`, `save_count`, `deleted`, `design_name`, `preview_image`, `width`, `height`, `date_created`, `date_updated` FROM `designs`', array());
+		$select = DB::select('SELECT `id`, `user_id`, `save_count`, `deleted`, `design_name`, `preview_image`, `width`, `height`, `date_created`, `date_updated` FROM `designs` WHERE `deleted` = 0', array());
 
 		foreach ($select as $key => $value) {
 			array_push($tattoo_array, array(
