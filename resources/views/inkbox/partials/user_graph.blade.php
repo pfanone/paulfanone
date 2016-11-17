5b32d58d5e26843cbf39625397ac582310f00816
Paul <pfanone@gmail.com>
2016-11-17 14:20:21 -0500

    changin widths

--- resources/views/inkbox/partials/user_graph.blade.php
+++ resources/views/inkbox/partials/user_graph.blade.php
@@ -1,33 +1,33 @@
 <div class="col-xs-12">
 	<div class="row">
-		<div class="col-xs-12 col-sm-4 col-md-3 col-lg-2">
+		<div class="col-xs-12 col-sm-4 col-md-3">
 			<div id="user_graph" class="graph-div graph-div-full">
 				<canvas id="user_data_chart" width="400" height="400"></canvas>
 			</div>
 		</div>
 
-		<div class="col-xs-12 col-sm-4 col-lg-5">
+		<div class="col-xs-12 col-sm-4">
 			<div class="graph-div graph-div-half">
 				<h4>Users All Time</h4>
 				<h2 class="graph-numbers">{!! $user_data_array['All']['count'] !!}</h2>
 			</div>
 		</div>
 
-		<div class="col-xs-12 col-sm-4 col-lg-5">
+		<div class="col-xs-12 col-sm-4">
 			<div class="graph-div graph-div-half">
 				<h4>Users Today</h4>
 				<h2 class="graph-numbers">{!! $user_data_array['Day']['count'] !!}<span class="pull-right">{!! $count_difference['Day'] !!}</span></h2>
 			</div>
 		</div>
 
-		<div class="col-xs-12 col-sm-4 col-lg-5">
+		<div class="col-xs-12 col-sm-4">
 			<div class="graph-div graph-div-half">
 				<h4>Users This Week</h4>
 				<h2 class="graph-numbers">{!! $user_data_array['Week']['count'] !!}<span class="pull-right">{!! $count_difference['Week'] !!}</span></h2>
 			</div>
 		</div>
 
-		<div class="col-xs-12 col-sm-4 col-lg-5">
+		<div class="col-xs-12 col-sm-4">
 			<div class="graph-div graph-div-half">
 				<h4>Users This Month</h4>
 				<h2 class="graph-numbers">{!! $user_data_array['Month']['count'] !!}<span class="pull-right">{!! $count_difference['Month'] !!}</span></h2>
