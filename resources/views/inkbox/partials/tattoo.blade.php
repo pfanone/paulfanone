833c9ab9f943c95b8a21bace110416ad0387c58b
Paul <pfanone@gmail.com>
2016-11-17 15:06:13 -0500

    update

--- resources/views/inkbox/partials/tattoo.blade.php
+++ resources/views/inkbox/partials/tattoo.blade.php
@@ -2,7 +2,7 @@
 @foreach($tattoos as $key => $tattoo)
 	<div class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
 		<div class="well">
-			{{ $tattoo['design_name'] }}
+			{{ json_encode($tattoo) }}
 		</div>
 	</div>
 @endforeach
