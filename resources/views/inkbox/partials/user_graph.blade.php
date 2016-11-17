49d93ef8348c36ea2644d98d8e61fdc2de339754
Paul <pfanone@gmail.com>
2016-11-17 14:24:05 -0500

    updates

--- resources/views/inkbox/partials/user_graph.blade.php
+++ resources/views/inkbox/partials/user_graph.blade.php
@@ -1,6 +1,6 @@
 <div class="col-xs-12">
 	<div class="row">
-		<div class="col-xs-12 col-sm-4 col-md-3">
+		<div class="col-xs-12 col-sm-4 col-md-4 col-lg-3">
 			<div id="user_graph" class="graph-div graph-div-full">
 				<canvas id="user_data_chart" width="400" height="400"></canvas>
 			</div>
