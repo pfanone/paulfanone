a1fe9e77c1d4158473c031cac4d049841b9e2264
Paul <pfanone@gmail.com>
2016-11-17 15:32:16 -0500

    update

--- resources/views/inkbox/partials/tattoo.blade.php
+++ resources/views/inkbox/partials/tattoo.blade.php
@@ -2,7 +2,29 @@
 @foreach($tattoo_array as $key => $tattoo)
 	<div class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
 		<div class="tattoo-div">
-			{!! $tattoo['design_name'] !!}
+			<div class="row">
+				<div class="col-xs-12">
+					<h2>{!! $tattoo['design_name'] !!}</h2>
+				</div>
+				<div class="col-xs-12">
+					<img src="{!! $tattoo['preview_image'] !!}">
+				</div>
+				<div class="col-xs-12">
+					<div class="row">
+						<div class="col-xs-12">
+							<p>{!! $tattoo['width'] !!}&nbsp;<i class="fa fa-times" aria-hidden="true"></i>&nbsp;{!! $tattoo['height'] !!}</p>
+						</div>
+						<div class="col-xs-12">
+							<p>Status: @if ($tattoo['deleted'] == 1)
+							Deleted
+							@else
+							Active
+							@endif
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 @endforeach
