0331fc63f0c3e2691f9df6a7734731859fd1f0aa
Paul <pfanone@gmail.com>
2016-11-17 11:52:41 -0500

    adding colour

--- resources/views/inkbox/partials/user_graph.blade.php
+++ resources/views/inkbox/partials/user_graph.blade.php
@@ -16,21 +16,21 @@
 		<div class="col-xs-12 col-sm-4 col-lg-5">
 			<div class="graph-div graph-div-half">
 				<h4>Users Today</h4>
-				<h2>{!! $user_data_array['Day']['count'] !!} {!! $count_difference['Day'] !!}</h2>
+				<h2>{!! $user_data_array['Day']['count'] !!}<span class="pull-right">{!! $count_difference['Day'] !!}</span></h2>
 			</div>
 		</div>
 
 		<div class="col-xs-12 col-sm-4 col-lg-5">
 			<div class="graph-div graph-div-half">
 				<h4>Users This Week</h4>
-				<h2>{!! $user_data_array['Week']['count'] !!} {!! $count_difference['Week'] !!}</h2>
+				<h2>{!! $user_data_array['Week']['count'] !!}<span class="pull-right">{!! $count_difference['Week'] !!}</span></h2>
 			</div>
 		</div>
 
 		<div class="col-xs-12 col-sm-4 col-lg-5">
 			<div class="graph-div graph-div-half">
 				<h4>Users This Month</h4>
-				<h2>{!! $user_data_array['Month']['count'] !!} {!! $count_difference['Month'] !!}</h2>
+				<h2>{!! $user_data_array['Month']['count'] !!}<span class="pull-right">{!! $count_difference['Month'] !!}</span></h2>
 			</div>
 		</div>
 	</div>
