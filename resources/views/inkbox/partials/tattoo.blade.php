4572b99a1a4b2a899f599cd93cf97543c777e258
Paul <pfanone@gmail.com>
2016-11-17 16:11:45 -0500

    update

--- resources/views/inkbox/partials/tattoo.blade.php
+++ resources/views/inkbox/partials/tattoo.blade.php
@@ -12,7 +12,7 @@
 				<div class="col-xs-12">
 					<div class="row">
 						<div class="col-xs-12">
-							<dl>
+							<dl class="marginT5">
 								<dt>Size:</dt>
 								<dd>{!! $tattoo['width'] !!}&nbsp;<i class="fa fa-times" aria-hidden="true"></i>&nbsp;{!! $tattoo['height'] !!}</dd>
 								<dt>Status:</dt>
