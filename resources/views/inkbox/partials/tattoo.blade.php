36ca87f43b49c36f635c9f4f477b30994bc54569
Paul <pfanone@gmail.com>
2016-11-17 15:22:34 -0500

    update

--- resources/views/inkbox/partials/tattoo.blade.php
+++ resources/views/inkbox/partials/tattoo.blade.php
@@ -1,7 +1,7 @@
 <div class="row">
 @foreach($tattoo_array as $key => $tattoo)
 	<div class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
-		<div class="graph-div">
+		<div class="tattoo-div">
 			{!! $tattoo['design_name'] !!}
 		</div>
 	</div>
