142a79009fe64bb324d973575e7edbe9af469d71
Paul <pfanone@gmail.com>
2016-11-17 16:17:04 -0500

    width update

--- resources/views/inkbox/partials/tattoo.blade.php
+++ resources/views/inkbox/partials/tattoo.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
 @foreach($tattoo_array as $key => $tattoo)
-	<div class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
+	<div class="col-xs-12 col-sm-4 col-md-3 col-lg-2">
 		<div class="tattoo-div">
 			<div class="row">
 				<div class="col-xs-12">
