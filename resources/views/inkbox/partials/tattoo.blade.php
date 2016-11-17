5048aad6696828598790d57f599a540d37143266
Paul <pfanone@gmail.com>
2016-11-17 15:51:50 -0500

    adding title

--- resources/views/inkbox/partials/tattoo.blade.php
+++ resources/views/inkbox/partials/tattoo.blade.php
@@ -4,7 +4,7 @@
 		<div class="tattoo-div">
 			<div class="row">
 				<div class="col-xs-12">
-					<h3 class="tattoo-header">{!! $tattoo['design_name'] !!}</h3>
+					<h3 class="tattoo-header" title="{!! $tattoo['design_name'] !!}">{!! $tattoo['design_name'] !!}</h3>
 				</div>
 				<div class="col-xs-12">
 					<div class="tattoo-img-container" style="background-image:url({!! $tattoo['preview_image'] !!});background-size: contain;background-repeat: no-repeat;"></div>
