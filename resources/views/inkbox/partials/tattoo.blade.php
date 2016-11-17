0563de698c19e321f2bde70dc2b4748c31892066
Paul <pfanone@gmail.com>
2016-11-17 15:17:51 -0500

    update

--- resources/views/inkbox/partials/tattoo.blade.php
+++ resources/views/inkbox/partials/tattoo.blade.php
@@ -1,3 +1,9 @@
 <div class="row">
-{!! json_encode($tattoo_array) !!}
+@foreach($tattoos as $key => $tattoo)
+	<div class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
+		<div class="graph-div">
+			{!! $tattoo['design_name'] !!}
+		</div>
+	</div>
+@endforeach
 </div>
