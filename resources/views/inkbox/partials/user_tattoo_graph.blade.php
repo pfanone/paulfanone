4733d03cb671b6c45e23a8cf78b8727fea24b66f
Paul <pfanone@gmail.com>
2016-11-17 19:22:32 -0500

    widths

--- resources/views/inkbox/partials/user_tattoo_graph.blade.php
+++ resources/views/inkbox/partials/user_tattoo_graph.blade.php
@@ -1,6 +1,6 @@
 <div class="col-xs-12">
 	<div class="row">
-		<div class="col-xs-12 col-sm-4">
+		<div class="col-xs-12 col-sm-8">
 			<div id="user_tattoo_graph" class="graph-div graph-div-full">
 				<canvas id="user_tattoo_data_chart" width="400" height="300"></canvas>
 			</div>
